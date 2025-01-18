4d85a6db7e6f148a6cc860cf879c913e1660ab29
oratake <smith.shimomura@gmail.com>
2025-01-18 22:39:42 +0900

    3.5.2 sqlxでDB接続

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,33 @@ use anyhow::Result;
 use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
-// async fn hello_world() -> &'static str {
-//     "Hello, world!"
-// }
+use sqlx::{postgres::PgConnectOptions, Database, PgPool};
+
+// DBの接続設定を表す構造体
+struct DatabaseConfig {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database: String,
+}
+
+// アプリケーション用のDB設定構造体からPostgres接続用の構造体に変換
+impl From<DatabaseConfig> for PgConnectOptions {
+    fn from(cfg: DatabaseConfig) -> self {
+        Self::new()
+            .host(&cfg.host)
+            .port(cfg.port)
+            .username(&cfg.username)
+            .password(&cfg.password)
+            .database(&cfg.database)
+    }
+}
+
+// Postgres専用のコネクションプールを作成
+fn connect_database_with(cfg: DatabaseConfig) -> PgPool {
+    PgPool::connect_lazy_with(cfg.into())
+}
 
 pub async fn health_check() -> StatusCode {
     StatusCode::OK
@@ -14,6 +38,15 @@ pub async fn health_check() -> StatusCode {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let database_cfg = DatabaseConfig {
+        host: "localhost".into(),
+        port: 5432,
+        username: "app".into(),
+        password: "passwd".into(),
+        database: "app".into(),
+    };
+    let conn_pool = connect_database_with(database_cfg);
+
     let app = Router::new().route("/health", get(health_check));
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(addr).await?;

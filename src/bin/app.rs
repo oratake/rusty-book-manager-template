b21901b12f05507c9356b51e9ab36b63889a232c
oratake <smith.shimomura@gmail.com>
2025-02-02 00:42:55 +0900

    4章おわり ヘルスチェック部分をレイヤードアーキテクチャに

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,79 +1,73 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use anyhow::Result;
-use axum::{extract::State, http::StatusCode, routing::get, Router};
+use adapter::database::connect_database_with;
+use anyhow::{Error, Result};
+use api::route::health::build_health_check_routers;
+use axum::Router;
+use registry::AppRegistry;
+use shared::config::AppConfig;
 use tokio::net::TcpListener;
-
-use sqlx::{postgres::PgConnectOptions, Database, PgPool};
-
-// DBの接続設定を表す構造体
-struct DatabaseConfig {
-    pub host: String,
-    pub port: u16,
-    pub username: String,
-    pub password: String,
-    pub database: String,
-}
-
-// アプリケーション用のDB設定構造体からPostgres接続用の構造体に変換
-impl From<DatabaseConfig> for PgConnectOptions {
-    fn from(cfg: DatabaseConfig) -> Self {
-        Self::new()
-            .host(&cfg.host)
-            .port(cfg.port)
-            .username(&cfg.username)
-            .password(&cfg.password)
-            .database(&cfg.database)
-    }
-}
-
-// Postgres専用のコネクションプールを作成
-fn connect_database_with(cfg: DatabaseConfig) -> PgPool {
-    PgPool::connect_lazy_with(cfg.into())
-}
-
-pub async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let database_cfg = DatabaseConfig {
-        host: "localhost".into(),
-        port: 5432,
-        username: "app".into(),
-        password: "passwd".into(),
-        database: "app".into(),
-    };
-    let conn_pool = connect_database_with(database_cfg);
+    bootstrap().await
+}
 
+// ログ初期化など他の関数をmain関数に挟むため今のうちにサーバ起動分だけ分離
+async fn bootstrap() -> Result<()> {
+    let app_config = AppConfig::new()?; //AppConfig生成
+    let pool = connect_database_with(&app_config.database); //DB接続(コネクションプールの取り出し)
+    let registry = AppRegistry::new(pool); // AppRegistryを生成
+
+    // build_health_check_routers関数の呼び出し、AppRegistryをRouterに登録
     let app = Router::new()
-        .route("/health", get(health_check))
-        .route("/health/db", get(health_check_db))
-        // ルータのStateにプールを登録、各ハンドラで利用できるように
-        .with_state(conn_pool);
+        .merge(build_health_check_routers())
+        .with_state(registry);
+
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await?;
+    let listener = TcpListener::bind(&addr).await?;
+
     println!("Listening on {}", addr);
-    Ok(axum::serve(listener, app).await?)
+
+    axum::serve(listener, app).await.map_err(Error::from)
 }
 
-#[tokio::test]
-async fn health_check_works() {
-    let status_code = health_check().await;
-    assert_eq!(status_code, StatusCode::OK);
-}
-
-async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
-    let connection_result = sqlx::query("SELECT 1").fetch_one(&db).await;
-    match connection_result {
-        Ok(_) => StatusCode::OK,
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
-    }
-}
-
-#[sqlx::test]
-async fn health_check_db_works(pool: sqlx::PgPool) {
-    let status_code = health_check_db(State(pool)).await;
-    assert_eq!(status_code, StatusCode::OK);
-}
+//     let database_cfg = DatabaseConfig {
+//         host: "localhost".into(),
+//         port: 5432,
+//         username: "app".into(),
+//         password: "passwd".into(),
+//         database: "app".into(),
+//     };
+//     let conn_pool = connect_database_with(database_cfg);
+//
+//     let app = Router::new()
+//         .route("/health", get(health_check))
+//         .route("/health/db", get(health_check_db))
+//         // ルータのStateにプールを登録、各ハンドラで利用できるように
+//         .with_state(conn_pool);
+//     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+//     let listener = TcpListener::bind(addr).await?;
+//     println!("Listening on {}", addr);
+//     Ok(axum::serve(listener, app).await?)
+// }
+//
+// #[tokio::test]
+// async fn health_check_works() {
+//     let status_code = health_check().await;
+//     assert_eq!(status_code, StatusCode::OK);
+// }
+//
+// async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
+//     let connection_result = sqlx::query("SELECT 1").fetch_one(&db).await;
+//     match connection_result {
+//         Ok(_) => StatusCode::OK,
+//         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+//     }
+// }
+//
+// #[sqlx::test]
+// async fn health_check_db_works(pool: sqlx::PgPool) {
+//     let status_code = health_check_db(State(pool)).await;
+//     assert_eq!(status_code, StatusCode::OK);
+// }
